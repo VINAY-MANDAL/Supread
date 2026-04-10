449f8fce85753809350b56cb6005fcfc899f0a85
Vinay Kumar Mandal <vinaykumarmandal847227@gmail.com>
2026-04-10 12:19:16 +0530

    sacnner feature

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_doc_scanner
   syncfusion_pdfviewer_linux
   url_launcher_linux
 )
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_doc_scanner
   syncfusion_pdfviewer_windows
   url_launcher_windows
 )
