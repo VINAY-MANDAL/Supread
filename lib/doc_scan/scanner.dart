7a529cef18fc8af7087866159bd4172a1c2ffc60
Vinay Kumar Mandal <vinaykumarmandal847227@gmail.com>
2026-04-10 14:09:23 +0530

    scanner feature modify

--- lib/doc_scan/scanner.dart
+++ lib/doc_scan/scanner.dart
@@ -1,1 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_doc_scanner/flutter_doc_scanner.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  dynamic _scannedDocuments;
+
+  Future<void> scanDocument() async {
+    dynamic scannedDocuments;
+    try {
+      scannedDocuments = await FlutterDocScanner().getScanDocuments(page: 4) ??
+          'Unknown platform documents';
+    } on PlatformException {
+      scannedDocuments = 'Failed to get scanned documents.';
+    }
+    print(scannedDocuments.toString());
+    if (!mounted) return;
+    setState(() {
+      _scannedDocuments = scannedDocuments;
+    });
+  }
+
+  Future<void> scanDocumentAsImages() async {
+    dynamic scannedDocuments;
+    try {
+      scannedDocuments =
+          await FlutterDocScanner().getScannedDocumentAsImages(page: 4) ??
+              'Unknown platform documents';
+    } on PlatformException {
+      scannedDocuments = 'Failed to get scanned documents.';
+    }
+    print(scannedDocuments.toString());
+    if (!mounted) return;
+    setState(() {
+      _scannedDocuments = scannedDocuments;
+    });
+  }
+
+  Future<void> scanDocumentAsPdf() async {
+    dynamic scannedDocuments;
+    try {
+      scannedDocuments =
+          await FlutterDocScanner().getScannedDocumentAsPdf(page: 4) ??
+              'Unknown platform documents';
+    } on PlatformException {
+      scannedDocuments = 'Failed to get scanned documents.';
+    }
+    print(scannedDocuments.toString());
+    if (!mounted) return;
+    setState(() {
+      _scannedDocuments = scannedDocuments;
+    });
+  }
+
+  Future<void> scanDocumentUri() async {
+    //This Feature only supported for Android.
+    dynamic scannedDocuments;
+    try {
+      scannedDocuments =
+          await FlutterDocScanner().getScanDocumentsUri(page: 4) ??
+              'Unknown platform documents';
+    } on PlatformException {
+      scannedDocuments = 'Failed to get scanned documents.';
+    }
+    print(scannedDocuments.toString());
+    if (!mounted) return;
+    setState(() {
+      _scannedDocuments = scannedDocuments;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Document Scanner Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Document Scanner example app'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _scannedDocuments != null
+                    ? Text(_scannedDocuments.toString())
+                    : const Text("No Documents Scanned"),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    scanDocument();
+                  },
+                  child: const Text("Scan Documents"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    scanDocumentAsImages();
+                  },
+                  child: const Text("Scan Documents As Images"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    scanDocumentAsPdf();
+                  },
+                  child: const Text("Scan Documents As PDF"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    scanDocumentUri();
+                  },
+                  child: const Text("Get Scan Documents URI"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
