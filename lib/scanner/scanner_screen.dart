40c6e1c0d652377d12d9f85379925b239373fb6b
Vinay Kumar Mandal <vinaykumarmandal847227@gmail.com>
2026-04-18 08:40:55 +0530

    modified

--- lib/scanner/scanner_screen.dart
+++ lib/scanner/scanner_screen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_doc_scanner/flutter_doc_scanner.dart';
-import 'package:open_filex/open_filex.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
